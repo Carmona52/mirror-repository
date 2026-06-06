a05d4381b9fecc0f2c9b0dd85ac4abbef7cf3d9f
Jesus Carmona <ryzen3000@outlook.es>
2026-06-06 01:22:52 -0600

    vete alv era un pinche letra minuscula

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const fuenteLocal = localFont({
         {path: './fonts/NotoSans-Light.ttf', weight: '300', style: 'normal'},
         {path: './fonts/NotoSans-Regular.ttf', weight: '400', style: 'normal'},
         {path: './fonts/NotoSans-Medium.ttf', weight: '500', style: 'normal'},
-        {path: './fonts/NotoSans-Semibold.ttf', weight: '600', style: 'normal'},
+        {path: './fonts/NotoSans-SemiBold.ttf', weight: '600', style: 'normal'},
         {path: './fonts/NotoSans-Bold.ttf', weight: '700', style: 'normal'},
         {path: './fonts/NotoSans-ExtraBold.ttf', weight: '800', style: 'normal'},
         {path: './fonts/NotoSans-Black.ttf', weight: '900', style: 'normal'},
